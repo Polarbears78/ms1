3333ed4e1859b154b585a8c59b5f4346af0b25d7
Polarbears78 <91547292+Polarbears78@users.noreply.github.com>
2026-08-19 14:07:59 +0900

    Update config.js

--- taeyang/assets/config.js
+++ taeyang/assets/config.js
@@ -1,2 +1,2 @@
-// Apps Script 웹 앱 배포 URL을 여기에 붙여넣으세요. (SETUP.md 3단계)
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxYidgVlpTjXGxsQNb2DFGAyfOdNsAaWDOQALWhYj3hZLuGHSTDebdY3Ti7RaH-MVmw/exec";
+// Apps Script 웹 앱 주소 (태양계 · Ⅰ단원 공용)
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTGSW1owJ9c3vSaKEvNuywvduc5-2mDy7HS3Nc06hl0nqzH4NphGkTTy2_mIe4718O/exec";
