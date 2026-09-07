a65c0b897913eef6cc8df58b9f96d4889cf36e78
Polarbears78 <91547292+Polarbears78@users.noreply.github.com>
2026-09-07 13:34:53 +0900

    Update config.js

--- saengmul/assets/config.js
+++ saengmul/assets/config.js
@@ -1,4 +1,4 @@
 /* Apps Script 웹 앱 주소
    taeyang 폴더의 assets/config.js와 동일한 주소를 사용합니다.
    taeyang/assets/config.js를 이 파일 위에 그대로 복사해 덮어쓰면 됩니다. */
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTGSW1owJ9c3vSaKEvNuywvduc5-2mDy7HS3Nc06hl0nqzH4NphGkTTy2_mIe4718O/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxDGROQIgJcDK_fJmqZVdgV3tzMB4t_bYXhmI8llmZJNpDYSwB03L6PvVXG9TdDy3AlUw/exec";
