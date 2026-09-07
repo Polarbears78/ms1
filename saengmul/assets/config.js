50b1bb652491e48a4ffbd6e7e9535b327dbbfe17
Polarbears78 <91547292+Polarbears78@users.noreply.github.com>
2026-09-07 12:45:12 +0900

    Change SCRIPT_URL to new web app address
    Updated SCRIPT_URL to the new Google Apps Script web app URL.

--- saengmul/assets/config.js
+++ saengmul/assets/config.js
@@ -1,4 +1,4 @@
 /* Apps Script 웹 앱 주소
    taeyang 폴더의 assets/config.js와 동일한 주소를 사용합니다.
    taeyang/assets/config.js를 이 파일 위에 그대로 복사해 덮어쓰면 됩니다. */
-const SCRIPT_URL = "여기에_APPS_SCRIPT_웹앱_URL";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTGSW1owJ9c3vSaKEvNuywvduc5-2mDy7HS3Nc06hl0nqzH4NphGkTTy2_mIe4718O/exec";
